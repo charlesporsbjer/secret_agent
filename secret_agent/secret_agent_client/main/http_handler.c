ced478dd8350a2015a233538086eecd86e15d9f6
Patraxx <patrik.cederlund@gmail.com>
2025-01-16 11:17:10 +0100

    ploppp

--- secret_agent/secret_agent_client/main/http_handler.c
+++ secret_agent/secret_agent_client/main/http_handler.c
@@ -68,7 +68,8 @@ esp_err_t http_event_handler(esp_http_client_event_t *evt){
             ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
             if (output_buffer != NULL) {
                 printf("Player ID: %.*s\n", output_len, output_buffer);
-                playerID = (char *)malloc(output_len);            
+                memcpy(playerID, output_buffer, MIN(output_len, sizeof(playerID) - 1));
+                playerID[MIN(output_len, sizeof(playerID) - 1)] = '\0'; // Ensure null-termination
                 free(output_buffer);
                 output_buffer = NULL;
             }
