6b155d3520a5549229e0bccd178491943d56f2a0
Patraxx <patrik.cederlund@gmail.com>
2025-01-26 10:14:16 +0100

    dataparsefrommqtt?

--- secret_agent/secret_agent_client/main/mqtt_handler.c
+++ secret_agent/secret_agent_client/main/mqtt_handler.c
@@ -25,13 +25,9 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         char* test_msg = "Hello from ESP32";
         mqtt_subscribe(client);
         esp_mqtt_client_publish(client, "/torget", test_msg, strlen(test_msg), 1, 0);
-
-        // fyll ut
-
         break;
     case MQTT_EVENT_DISCONNECTED:
      //   PRINTFC_MQTT("MQTT_EVENT_DISCONNECTED");
-
         break;
     case MQTT_EVENT_SUBSCRIBED:
      //   PRINTFC_MQTT("MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);     
@@ -40,11 +36,12 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
      //   PRINTFC_MQTT("MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_PUBLISHED:
-     //   PRINTFC_MQTT("MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        PRINTFC_MQTT("MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_DATA:
        PRINTFC_MQTT("MQTT_EVENT_DATA");
        PRINTFC_MQTT("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+       PRINTFC_MQTT("DATA=%.*s\r\n", event->data_len, event->data);
         break;
    case MQTT_EVENT_ERROR:
             ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
